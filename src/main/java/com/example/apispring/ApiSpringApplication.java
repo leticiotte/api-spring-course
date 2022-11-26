831fbb3dd32066e48c738740fbe8a36c6ef68b9c
Letícia Gonçalves <leticiaespregagon@gmail.com>
2022-11-26 20:24:07 -0300

    feat: cria classe User

--- src/main/java/com/example/apispring/ApiSpringApplication.java
+++ src/main/java/com/example/apispring/ApiSpringApplication.java
@@ -1,5 +1,6 @@
 package com.example.apispring;
 
+import com.example.apispring.domain.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,8 @@ public class ApiSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiSpringApplication.class, args);
+
+		User user = new User(1, "Letícia", "leticia@gmail.com", "senha123");
 	}
 
 }
