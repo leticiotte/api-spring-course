89c79425ea1b91440761a1dd690d5ce250791ac7
Letícia Gonçalves <leticiaespregagon@gmail.com>
2022-12-04 22:54:01 -0300

    adiciona service ao controller de user no método findById

--- src/main/java/com/example/apispring/controllers/UserController.java
+++ src/main/java/com/example/apispring/controllers/UserController.java
@@ -1,6 +1,8 @@
 package com.example.apispring.controllers;
 
 import com.example.apispring.domain.User;
+import com.example.apispring.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,10 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
+    @Autowired
+    private UserService service;
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id){
-
-        return ResponseEntity.ok().body(new User(null, "Letícia Gonçalves", "leticiaespregagon@gmail.com", "senha123"));
+        return ResponseEntity.ok().body(this.service.findById(id));
     }
 }
