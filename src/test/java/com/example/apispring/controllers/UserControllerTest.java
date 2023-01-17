9bb4d47c8d23bbcca7cb730672d4e48d12076b5f
Letícia Gonçalves <leticiaespregagon@gmail.com>
2023-01-17 15:47:15 -0300

    feat: implementa teste para classe user controller - método findById

--- src/test/java/com/example/apispring/controllers/UserControllerTest.java
+++ src/test/java/com/example/apispring/controllers/UserControllerTest.java
@@ -3,13 +3,16 @@ package com.example.apispring.controllers;
 import com.example.apispring.domain.User;
 import com.example.apispring.domain.dto.UserDTO;
 import com.example.apispring.services.impl.UserServiceImpl;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
 @SpringBootTest
 class UserControllerTest {
@@ -35,7 +38,22 @@ class UserControllerTest {
     }
 
     @Test
-    void findById() {
+    void whenFindByIdThenReturnSuccess() {
+        Mockito.when(this.service.findById(Mockito.anyInt())).thenReturn(user);
+        Mockito.when(this.mapper.map(Mockito.any(), Mockito.any())).thenReturn(userDTO);
+
+        ResponseEntity<UserDTO> response = this.controller.findById(ID);
+
+        Assertions.assertNotNull(response);
+        Assertions.assertNotNull(response.getBody());
+        Assertions.assertEquals(200, response.getStatusCode().value());
+        Assertions.assertEquals(ResponseEntity.class, response.getClass());
+        Assertions.assertEquals(UserDTO.class, response.getBody().getClass());
+
+        Assertions.assertEquals(ID, response.getBody().getId());
+        Assertions.assertEquals(NAME, response.getBody().getName());
+        Assertions.assertEquals(EMAIL, response.getBody().getEmail());
+        Assertions.assertEquals(PASSWORD, response.getBody().getPassword());
     }
 
     @Test
